f43e84e180068ecc056b46ec60a5ef9173f5a7fb
Mahisharma000 <sharma02mahi@gmail.com>
2024-02-11 23:03:19 +0530

    changes maade

--- EulerPhiAlgorithm.java
+++ EulerPhiAlgorithm.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
-
-public class EulerPhiAlgorithm {
-    public static int phi(int n) {
-        float result = n;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                while (n % i == 0) {
-                    n /= i;
-                    result *= (float) (1.0 - (1.0 / i));
-                }
-            }
-        }
-        if (n > 1) {
-            result -= result / n;
-        }
-        return (int) result;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(phi(n));
-    }
+import java.io.*;
+class Main
+{
+static int phi(int n)
+{
+ int result = n;
+ for (int p = 2; p * p <= n; ++p)
+ { 
+ if (n % p == 0)
+ {
+ while (n % p == 0)
+ n /= p;
+ result -= result / p;
+}}
+ 
+ if (n > 1)
+ result -= result / n;
+ return result;
+}
+public static void main (String[] 
+args)
+{
+ int n=35;
+ System.out.println(phi(n));
+}
 }
